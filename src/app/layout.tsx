abdd0eee4f55a924f23b9e00a0d383105c74f2fb
Peter Muthama <muthamapeter79@gmail.com>
2025-04-10 16:31:06 +0300

    update, openGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/component/footer";
@@ -6,7 +5,22 @@ import Nav from "@/component/nav";
 
 export const metadata: Metadata = {
   title: "Menucha craft",
-  description: "Menucha craft",
+  description: "Christian stationery & gifts",
+  openGraph: {
+    title: "Menucha craft",
+    description: "Christian stationery & gifts",
+    url: "https://menuchacraft.vercel.app",
+    siteName: "Menucha craft",
+    images: [{ url: "/preview.png", width: 1200, height: 630 }],
+    locale:"en_KE",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Menucha craft",
+    description: "Christian stationery & gifts",
+    images: "/preview.png",
+  },
   icons: {
     icon: "/favicon.ico",
   },
@@ -17,17 +31,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
- 
   return (
     <html lang="en">
-      <head>
-        
-      </head>
-      <body
-      >
-        <Nav/>
+      <head></head>
+      <body>
+        <Nav />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
