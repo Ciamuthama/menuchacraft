6570d9dff54435ba3d34f4b2401f7c71431c064e
Peter Muthama <muthamapeter79@gmail.com>
2025-04-16 12:53:27 +0300

    sitemap reconfigure

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,5 @@ export default {
   siteUrl: 'https://menucha.co.ke',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  additionalSitemaps: [
-    'https://menucha.co.ke/dynamic-sitemap.json'
-  ]
+ 
 }
