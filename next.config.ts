25ca7ac8ce2b1f775ac20d284cab2f040599577a
Peter Muthama <muthamapeter79@gmail.com>
2025-04-24 17:17:14 +0300

    fixed caching problem

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = withPWA(nextConfig);
